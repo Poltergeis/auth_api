56a86095dc8259a4819c052973db8da602438d70
Poltergeis <julio.cruzazul@hotmail.com>
2025-12-04 10:28:05 -0600

    sin dist

--- dist/domain/Errors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserAlreadyExistsError = exports.InvalidCredentialsError = void 0;
-class InvalidCredentialsError extends Error {
-}
-exports.InvalidCredentialsError = InvalidCredentialsError;
-class UserAlreadyExistsError extends Error {
-}
-exports.UserAlreadyExistsError = UserAlreadyExistsError;
